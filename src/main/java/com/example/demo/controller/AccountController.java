594fdb83b5ef3db58e774e4a66fe5a1a4377aeee
villageworks <villageworks@gmail.com>
2023-11-11 23:32:08 +0900

    ver.0.02: 会員登録と会員内容確認画面の作成

--- src/main/java/com/example/demo/controller/AccountController.java
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -1,8 +1,10 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AccountController {
@@ -13,7 +15,16 @@ public class AccountController {
 	}
 	
 	@PostMapping("/account/confirm")
-	public String confirm() {
+	public String confirm(
+			@RequestParam(name = "name", defaultValue = "") String name,
+			@RequestParam(name = "email", defaultValue = "") String email,
+			@RequestParam(name = "password", defaultValue = "") String password,
+			Model model) {
+		// リクエストパラメータをスコープに登録
+		model.addAttribute("name", name);
+		model.addAttribute("email", email);
+		model.addAttribute("password", password);
+		// 画面遷移
 		return "accountConfirm";
 	}
 }
